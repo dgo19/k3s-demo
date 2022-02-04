809c92c32eecbc1dd0418895cbf1ce991e4ca10d
Daniel Gottschalk <dgo@gmx.net>
2022-02-04 19:20:56 +0100

    monitoring: still trying to find suitable loki dashboard

--- applications/monitoring/download_loki_dashboards.sh
+++ applications/monitoring/download_loki_dashboards.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-LOKI_DASHBOARDS="13186 12611"
+set -x
+LOKI_DASHBOARDS="12019,2"
 
-for DBID in ${LOKI_DASHBOARDS}; do
-	curl -skf "https://grafana.com/api/dashboards/${DBID}/revisions/1/download" | sed '/-- .* --/! s/"datasource":.*,/"datasource": "loki",/g' | sed 's/pod_name/pod/g' | sed 's/container_name/container/g' > dashboard-${DBID}.json
+for DBIDREV in ${LOKI_DASHBOARDS}; do
+	DBID=$(echo ${DBIDREV} | cut -d "," -f1)
+	DBREV=$(echo ${DBIDREV} | cut -d "," -f2)
+	curl -skf "https://grafana.com/api/dashboards/${DBID}/revisions/${DBREV}/download" | sed '/-- .* --/! s/"datasource":.*,/"datasource": "loki",/g' | sed 's/pod_name/pod/g' | sed 's/container_name/container/g' > dashboard-${DBID}.json
 	kubectl -n monitoring create configmap grafana-dashboard-${DBID} --dry-run=client -o yaml --from-file=dashboard-${DBID}.json | sed '/^metadata:.*/a \  labels:\n    grafana_dashboard: "1"' > templates/configmap-dashboard-${DBID}.yaml
 	rm dashboard-${DBID}.json
 done
