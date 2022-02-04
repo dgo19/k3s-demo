931c7dfa3073fdc55586c0ad6e28d5de8cb1d61e
Daniel Gottschalk <dgo@gmx.net>
2022-02-04 19:07:26 +0100

    monitoring: removes broken dashboards for loki and updates loki dashboard revision

--- applications/monitoring/download_loki_dashboards.sh
+++ applications/monitoring/download_loki_dashboards.sh
@@ -4,4 +4,5 @@ LOKI_DASHBOARDS="13186 12611"
 for DBID in ${LOKI_DASHBOARDS}; do
 	curl -skf "https://grafana.com/api/dashboards/${DBID}/revisions/1/download" | sed '/-- .* --/! s/"datasource":.*,/"datasource": "loki",/g' | sed 's/pod_name/pod/g' | sed 's/container_name/container/g' > dashboard-${DBID}.json
 	kubectl -n monitoring create configmap grafana-dashboard-${DBID} --dry-run=client -o yaml --from-file=dashboard-${DBID}.json | sed '/^metadata:.*/a \  labels:\n    grafana_dashboard: "1"' > templates/configmap-dashboard-${DBID}.yaml
+	rm dashboard-${DBID}.json
 done
